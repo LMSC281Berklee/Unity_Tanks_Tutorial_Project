be4d314b8cc2490efe0d9d85a883baf9ebc673d1
cwrobertson16 <cwrobertson@berklee.edu>
2016-11-27 14:47:11 -0500

    Added Custom Function
    Custom function added to unmute audio

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -4,13 +4,26 @@ using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour {
 
+	GameObject GameManagerAudio;
+
 	// Use this for initialization
 	void Start () {
-	
+
+		GameManagerAudio = GameObject.Find ("GameManager");
+
+		GetComponent<AudioSource> ().mute = true; //Initialize the game by having the music muted - Conrad Robertson
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	//Conrad Robertson
+	public static void PlayMusic (GameObject GameManagerAudio){
+
+		GameManagerAudio.GetComponent<AudioSource> ().mute = false;
+
 	}
 }
